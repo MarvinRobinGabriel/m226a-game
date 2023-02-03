97483d1f06881a26c703785597599340fc2d1b06
Marvin Gabriel <m.gabriel@rafisa.ch>
2023-02-03 12:01:40 +0100

    MISC: Implemented Game Strcuture & GameEngine Functionality

--- code/src/App.java
+++ code/src/App.java
@@ -1,9 +1,42 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-
-    
-
+        new App().start();
     }
 
- 
+    public void start() {    
+        GameEngine.printSegment("title");
+        menu();
+    }
+
+    public static void menu() {
+        Scanner sc = new Scanner(System.in);
+        int choice;
+        boolean repeat = true;
+
+        while ( repeat ) {
+            GameEngine.printMessage("blue", "Please choose a number to continue");
+            GameEngine.printMessage("blue", "1) Start Game");
+            GameEngine.printMessage("blue", "2) About this Project:");
+            choice = sc.nextInt();
+
+            switch(choice){
+                case 1:
+                    repeat = false;
+                    GameEngine.locationFactory();
+                    GameEngine.createPlayer();
+                    break;
+                case 2:
+                    repeat = true;
+                    GameEngine.printSegment("about");
+                    break;
+                default:
+                    repeat = true;
+                    GameEngine.printMessage("red", "Please choose from the provided options");
+                    return;
+            }
+        }
+    }
 }
